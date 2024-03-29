11bf6de8ad877a932a9a5425a476dfe9e53af575
Erika Avilés <eriavicor34@gmail.com>
2024-03-29 15:37:17 -0500

    Cambio en el localLoader

--- components/localLoader.js
+++ components/localLoader.js
@@ -1,6 +1,6 @@
 // Docs: https://aws.amazon.com/developer/application-security-performance/articles/image-optimization
 export default function localLoader({ src, width, quality }) {
-  const url = new URL(`http://localhost:3000/${src}`);
+  const url = new URL(`${process.env.NEXT_PUBLIC_URL}/${src}`);
   url.searchParams.set("format", "auto");
   url.searchParams.set("width", width.toString());
   url.searchParams.set("quality", (quality || 75).toString());
