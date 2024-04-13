c458653519ce2d90e8db141e080795cb34005488
Erika Avilés <eriavicor34@gmail.com>
2024-04-13 11:23:00 -0500

    ORDENAMIEMTO DEL CARRITO POR ORDEN DE LLEGADA

--- pages/api/cart.js
+++ pages/api/cart.js
@@ -7,7 +7,14 @@ export default async function handle(req, res) {
   if (method === "POST") {
     try {
       const ids = req.body.ids;
-      res.json(await Product.find({ _id: ids }));
+
+      const products = await Product.find({ _id: { $in: ids } });
+
+      const sortedProducts = ids.map((id) => {
+        return products.find((product) => product._id == id);
+      });
+
+      return res.status(200).json(sortedProducts);
     } catch (err) {
       return res.status(500).json({ err: err.message });
     }
