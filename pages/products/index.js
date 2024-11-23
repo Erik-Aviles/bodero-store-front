1fd849827811710acfbdb7dfc566adee2282ece4
Erika Avilés <eriavicor34@gmail.com>
2024-11-23 16:02:26 -0500

    Aplicando iconos reales y vewrificacion de google

--- pages/products/index.js
+++ pages/products/index.js
@@ -1,20 +1,20 @@
-import { ButtonContainer } from "@/components/buttonComponents/ButtonContainer";
-import { useData } from "@/hooks/useData";
-import ButtonDisabled from "@/components/buttonComponents/ButtonDisabled";
-import SkeletorProducts from "@/components/skeletor/SkeletorProducts";
-import BackButton from "@/components/buttonComponents/BackButton";
-import Text from "@/components/stylesComponents/HighlightedText";
-import { FlexStyled } from "@/components/stylesComponents/Flex";
-import Title from "@/components/stylesComponents/Title";
-import ProductsGrid from "@/components/ProductsGrid";
-import filterSearch from "@/utils/filterSearch";
-import styled, { css } from "styled-components";
-import { grey, secondary } from "@/lib/colors";
-import useProducts from "@/hooks/useProducts";
-import { Loader } from "@/components/Loader";
-import { useEffect, useState } from "react";
-import { useRouter } from "next/router";
-import Layout from "@/components/Layout";
+import { ButtonContainer } from '@/components/buttonComponents/ButtonContainer'
+import { useData } from '@/hooks/useData'
+import ButtonDisabled from '@/components/buttonComponents/ButtonDisabled'
+import SkeletorProducts from '@/components/skeletor/SkeletorProducts'
+import BackButton from '@/components/buttonComponents/BackButton'
+import Text from '@/components/stylesComponents/HighlightedText'
+import { FlexStyled } from '@/components/stylesComponents/Flex'
+import Title from '@/components/stylesComponents/Title'
+import ProductsGrid from '@/components/ProductsGrid'
+import filterSearch from '@/utils/filterSearch'
+import styled, { css } from 'styled-components'
+import { grey, secondary } from '@/lib/colors'
+import useProducts from '@/hooks/useProducts'
+import { Loader } from '@/components/Loader'
+import { useEffect, useState } from 'react'
+import { useRouter } from 'next/router'
+import Layout from '@/components/Layout'
 
 const CenterDiv = styled.section`
   height: auto;
@@ -23,7 +23,7 @@ const CenterDiv = styled.section`
   @media screen and (min-width: 640px) {
     padding: 20px 40px;
   }
-`;
+`
 const FlexFooter = styled.div`
   width: 100%;
   display: Flex;
@@ -31,15 +31,15 @@ const FlexFooter = styled.div`
   justify-content: center;
   gap: 4px;
   padding-bottom: ;
-`;
+`
 
 export default function ProductsPage() {
-  const { company } = useData();
-  const brands = company?.brands;
+  const { company } = useData()
+  const brands = company?.brands
 
-  const router = useRouter();
-  const { query } = router;
-  const [limit] = useState(20);
+  const router = useRouter()
+  const { query } = router
+  const [limit] = useState(20)
   const {
     data,
     error,
@@ -48,29 +48,30 @@ export default function ProductsPage() {
     setPages,
     handleGoBack,
     handlePageChange,
-  } = useProducts(limit);
+  } = useProducts(limit)
 
   useEffect(() => {
     if (query.page) {
-      const newPage = parseInt(query.page, 10);
-      setPages(newPage);
-      filterSearch({ router, page: newPage });
+      const newPage = parseInt(query.page, 10)
+      setPages(newPage)
+      filterSearch({ router, page: newPage })
     }
-  }, [query.page]);
+  }, [router, query.page, setPages])
 
-  const totalPages = data ? Math.ceil(data?.totalProducts / limit) : 1;
+  const totalPages = data ? Math.ceil(data?.totalProducts / limit) : 1
 
-  const hasNextPage = data && data?.result === 20;
+  const hasNextPage = data && data?.result === 20
 
-  const brandNames = brands?.map((brand) => brand.name);
-  const brandNamesString = brandNames?.join(", ");
+  const brandNames = brands?.map((brand) => brand.name)
+  const brandNamesString = brandNames?.join(', ')
 
-  if (error) return <div>Error: {error.message}</div>;
+  if (error) return <div>Error: {error.message}</div>
 
   return (
     <Layout
-      title="B.R.D | Todos los Productos"
+      title='B.R.D | Todos los Productos'
       description={`Marcas reconocidas como: ${brandNamesString}`}
+      sity='/products'
     >
       <CenterDiv>
         <FlexStyled>
@@ -113,5 +114,5 @@ export default function ProductsPage() {
         )}
       </CenterDiv>
     </Layout>
-  );
+  )
 }
